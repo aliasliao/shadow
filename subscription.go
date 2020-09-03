4f682699c5305b913167c29ced08592f8fa4978f
Jinyuan Liao <liaojinyuan@zju.edu.cn>
2020-09-04 01:24:26 +0800

    add subscription.go

---
+++ subscription.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/golang/protobuf/jsonpb"
+
+func GetSubscriptionSS(url string) ([]byte, error) {
+	shadowsocksList, err := parseSS(url, false)
+	if err != nil {
+		return nil, err
+	}
+	config, err := (&jsonpb.Marshaler{}).MarshalToString(ssToConfig(shadowsocksList))
+	if err != nil {
+		return nil, err
+	}
+	return []byte(config), nil
+}
