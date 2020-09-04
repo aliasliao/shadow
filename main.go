72a72f94d19fc739aa830fb29752f77b2ac12baa
Jinyuan Liao <liaojinyuan@zju.edu.cn>
2020-09-05 01:31:35 +0800

    remove dependency of v2ray

--- main.go
+++ main.go
@@ -5,30 +5,17 @@ import (
 	"io/ioutil"
 	"log"
 
-	"github.com/golang/protobuf/jsonpb"
+	"github.com/aliasliao/shadow/utils"
 )
 
 func main() {
 	originType := flag.String("type", "ss", `type of the origin, can be "ss" or "ssr"`)
 	origin := flag.String("origin", "", `origin url`)
-	cache := flag.Bool("cache", true, `whether to load data from cache file`)
-	verbose := flag.Bool("verbose", false, `whether to show detail`)
 	flag.Parse()
 
-	shadowsocksList, err := parseSS(*origin, *cache)
-	if err != nil {
-		log.Fatalln("error:", err)
-	}
-	if *verbose {
-		for index, item := range shadowsocksList {
-			log.Println(index, item)
-		}
-	}
-	log.Println("total:", len(shadowsocksList))
-
-	config, err := (&jsonpb.Marshaler{Indent: "  "}).MarshalToString(ssToConfig(shadowsocksList))
+	config, err := utils.GetSubscriptionSS(*origin)
 	if err != nil {
 		log.Fatalln("marshal error:", err)
 	}
-	ioutil.WriteFile(*originType+"_config.json", []byte(config), 0755)
+	ioutil.WriteFile(*originType+"_config.json", config, 0755)
 }
