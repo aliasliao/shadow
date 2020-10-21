3b76b9c35edf7a78ba27215af029b335ffa60487
Jinyuan Liao <liaojinyuan@zju.edu.cn>
2020-10-21 23:56:47 +0800

    wip ui

--- web/app.tsx
+++ web/app.tsx
@@ -1,28 +1,98 @@
 import * as React from 'react'
+import { useEffect, useState } from 'react'
+import { HOST, noop } from './utils'
 
 export default function App() {
   return (
-    <table>
-      <thead>
-      <tr>
-        <td>rendered by react</td>
-        <td>word dddddd</td>
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-        <td>1</td>
-        <td>2</td>
-      </tr>
-      <tr>
-        <td>1</td>
-        <td>2</td>
-      </tr>
-      <tr>
-        <td>1</td>
-        <td>2</td>
-      </tr>
-      </tbody>
-    </table>
+    <div>
+      <Stats />
+      <StartApp />
+    </div>
+  )
+}
+
+function Stats() {
+  const [data, setData] = useState<{
+    NumGoroutine: number,
+    NumGC: number,
+    Alloc: string,
+    TotalAlloc: string,
+    Sys: string,
+    Mallocs: string,
+    Frees: string,
+    LiveObjects: string,
+    Uptime: number
+  } | null>(null)
+  const [error, setError] = useState<Error | null>(null)
+  const getStats = () => fetch(`${HOST}/getStats`)
+    .then(res => res.json())
+    .then(data => setData(data))
+    .catch(error => setError(error))
+  useEffect(() => { getStats().then(noop) }, [])
+
+  if (data === null && error === null) {
+    return <>Loading status...</>
+  }
+  return (
+    <div>
+      {error && (
+        <div>{error.message}</div>
+      )}
+      {data && (
+        <div>
+          {Object.keys(data).map(key => (
+            <div key={key} className="d-flex">
+              <div>{key}</div>
+              <div>{data[key]}</div>
+            </div>
+          ))}
+        </div>
+      )}
+      <button onClick={() => getStats()}>Get Stats</button>
+    </div>
+  )
+}
+
+function StartApp() {
+  const [data, setData] = useState<{} | null>(null)
+  const [error, setError] = useState<Error | null>(null)
+  const startApp = (values: {
+    url: string,
+    cache: boolean,
+    loglevel: string,
+  }) => fetch(`${HOST}/startApp`, {
+    method: 'POST',
+    body: JSON.stringify(values),
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+  }).then(res => res.json())
+    .then(data => setData(data))
+    .catch(error => setError(error))
+
+  if (data === null && error === null) {
+    return <>Starting App...</>
+  }
+  return (
+    // @ts-ignore
+    <form onSubmit={e => startApp(e.target)}>
+      <div>
+        <label htmlFor="url">url</label>
+        <input type="text" id="url" />
+      </div>
+      <div>
+        <label htmlFor="cache">cache</label>
+        <input type="checkbox" id="cache" />
+      </div>
+      <div>
+        <label htmlFor="loglevel">loglevel</label>
+        <select id="loglevel">
+          {['warning', 'error', 'debug'].map(level => (
+            <option key={level} value={level}>{level}</option>
+          ))}
+        </select>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
   )
 }
---
+++ web/utils.ts
@@ -0,0 +1,2 @@
+export const HOST = 'http://router.asus.com:3000'
+export const noop = () => {}
