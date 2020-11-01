43a43da7da7642f102f6cf669ade84e2d26ee7da
Jinyuan Liao <liaojinyuan@zju.edu.cn>
2020-11-02 00:04:15 +0800

    add routes script

--- scripts/install.sh
+++ scripts/install.sh
@@ -41,11 +41,6 @@ umount /www/require/modules/menuTree.js && mount -o bind /tmp/menuTree.js /www/r
 
 cd /tmp/mnt/liao/shadow
 
-# Copy executables
-# mkdir -p /tmp/shadow_app
-# cp v2ray v2ctl /tmp/shadow_app
-# cd /tmp/shadow_app
-
 # Starting v2ctl deamon...
 echo "Starting v2ctl deamon..."
 ./v2ctl server 1>>./v2ctl.log 2>&1 &
---
+++ scripts/routes.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+if [ "$1" = "set" ]; then
+  echo "setting routes..."
+  iptables -t nat -N V2RAY
+  # Ignore LANs and any other addresses you'd like to bypass the proxy
+  # See Wikipedia and RFC5735 for full list of reserved networks.
+  iptables -t nat -A V2RAY -d 0.0.0.0/8 -j RETURN
+  iptables -t nat -A V2RAY -d 10.0.0.0/8 -j RETURN
+  iptables -t nat -A V2RAY -d 127.0.0.0/8 -j RETURN
+  iptables -t nat -A V2RAY -d 169.254.0.0/16 -j RETURN
+  iptables -t nat -A V2RAY -d 172.16.0.0/12 -j RETURN
+  iptables -t nat -A V2RAY -d 192.168.0.0/16 -j RETURN
+  iptables -t nat -A V2RAY -d 224.0.0.0/4 -j RETURN
+  iptables -t nat -A V2RAY -d 240.0.0.0/4 -j RETURN
+  # redirect all other packets to v2ray
+  iptables -t nat -A V2RAY -p tcp -j REDIRECT --to-ports 1081
+
+  # only handle packets from lan
+  iptables -t nat -A PREROUTING -i br0 -j V2RAY
+  echo "done"
+
+  exit 0
+fi
+
+if [ "$1" = "restore" ]; then
+  echo "restoring routes..."
+  iptables -t nat -D PREROUTING -i br0 -j V2RAY
+  iptables -t nat -F V2RAY
+  iptables -t nat -X V2RAY
+  echo "done"
+
+  exit 0
+fi
+
+echo "usage: iptables set/restore"
+exit 1
