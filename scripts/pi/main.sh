51190563ac7324c8ca5e711de14664bafb285160
Jinyuan Liao <liaojinyuan@zju.edu.cn>
2020-11-22 00:14:01 +0800

    optimize main.sh

--- scripts/pi/main.sh
+++ scripts/pi/main.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-cd /home/pi/projects/v2ray-core
-./routes.sh start
-./v2ctl server 1>>./v2ctl.log 2>&1 &
-sleep 3
-curl -X POST -H "Content-Type: application/json" --data-binary "@./startApp.curl.json" http://localhost:3000/startApp
+main() {
+    sleep 1
+    cd /home/pi/projects/v2ray-core
+    sudo ./routes.sh start
+    sudo ./v2ctl server 1>>./v2ctl.log 2>&1 &
+    sleep 1
+    curl -X POST -H "Content-Type: application/json" --data-binary "@./startApp.curl.json" http://localhost:3000/startApp
+}
+
+main 2>&1 | logger -t "v2ray-core"
