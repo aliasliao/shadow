f947e7ddd2d898448dc8ee369200722342ff9f05
Jinyuan Liao <liaojinyuan@zju.edu.cn>
2020-12-12 01:25:40 +0800

    update script

--- scripts/pi/main.sh
+++ scripts/pi/main.sh
@@ -3,10 +3,11 @@
 main() {
     sleep 1
     cd /home/pi/projects/v2ray-core
-    sudo ./routes.sh start
     sudo ./v2ctl server 1>>./v2ctl.log 2>&1 &
-    sleep 1
+    sleep 3
     curl -X POST -H "Content-Type: application/json" --data-binary "@./startApp.curl.json" http://localhost:3000/startApp
+    sleep 30
+    curl -X POST -H "Content-Type: application/json" -d '{"index": 9}' http://localhost:3000/updateShadowIndex
 }
 
 main 2>&1 | logger -t "v2ray-core"
